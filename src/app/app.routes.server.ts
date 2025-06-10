14961cd33e94a26e923822d4334e2242142ecdbf
hegazy <ahmedhegazy2025@gmail.com>
2025-06-10 12:50:08 +0300

    Refactor routing and server configuration; add SSR support for contact and profile pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,20 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // {
+  //   path: '**',
+  //   renderMode: RenderMode.Prerender,
+  // },
   {
-    path: '**',
-    renderMode: RenderMode.Prerender,
+    path: 'about',
+    renderMode: RenderMode.Prerender, //  SSG
+  },
+  {
+    path: 'profile',
+    renderMode: RenderMode.Server, //  SSR
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Client, //  CSR
   },
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+];
